a12890e8b88e752716a7a47b33688b53ee6ce71e
Viktoriya Nykytenko <kitakiv@gmail.com>
2024-05-02 11:52:34 +0300

    feat: set up eslint

--- final-task/vite.config.ts
+++ final-task/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
+import eslint from 'vite-plugin-eslint'
 
 export default defineConfig({
     base: './',
 
-    plugins: [],
+    plugins: [eslint()],
 
     server: {
         port: 8080,
