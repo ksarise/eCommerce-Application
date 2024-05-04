070a4ff9bba1738908f79ed03cb239091e3fd37a
Andrey Ushakov <ushakov.andrey257686@gmail.com>
2024-05-04 15:41:53 +0500

    feat: set up prettier

--- final-task/.eslintrc.cjs
+++ final-task/.eslintrc.cjs
@@ -1,56 +1,57 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint-config-airbnb-base',
+    'eslint-config-airbnb-typescript/base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
     },
-    "extends": [
-        "eslint-config-airbnb-base",
-        "eslint-config-airbnb-typescript/base",
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    '@typescript-eslint/no-explicit-any': 2,
+  },
+  plugins: [
+    'eslint-plugin-prettier',
+    'prettier',
+    'import',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'no-console': 0,
+    'prettier/prettier': [
+      'warn',
+      {
+        endOfLine: 'auto',
+      },
     ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "project": './tsconfig.json',
-        "ecmaVersion": "latest",
-        "sourceType": "module",
-        "@typescript-eslint/no-explicit-any": 2,
+  },
+  settings: {
+    'import/resolver': {
+      typescript: true,
     },
-    "plugins": [
-        "eslint-plugin-prettier",
-        "prettier",
-        "import",
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "no-console": 0 ,
-        "prettier/prettier": [
-            "warn",
-            {
-              "endOfLine": "auto"
-            }
-          ]
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts'],
     },
-    "settings": {
-        "import/resolver": {
-          "typescript": true,
-        },
-        "import/parsers": {
-          "@typescript-eslint/parser": [".ts"],
-        },
-        "noInlineConfig": true,
-      }
-}
+    noInlineConfig: true,
+  },
+};
