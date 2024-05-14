1e6612e2b01cc5f4a608a8ceea25e9209b856a75
sergkrav.social@gmail.com <sergkrav.social@gmail.com>
2024-05-14 20:36:01 +0500

    feat: add base component generation and pregenerated common tags

---
+++ final-task/src/components/tags.ts
@@ -0,0 +1,153 @@
+import BaseComponentGenerator from './base-component';
+
+const div = (
+  classNames: string[],
+  content?: string,
+  attributes: { [key: string]: string } = {},
+): BaseComponentGenerator => {
+  return new BaseComponentGenerator({
+    tag: 'div',
+    classNames,
+    content,
+    attributes,
+  });
+};
+
+const input = (
+  classNames: string[],
+  eventCallback: () => void,
+  attributes: { [key: string]: string } = {},
+): HTMLInputElement => {
+  return new BaseComponentGenerator({
+    tag: 'input',
+    classNames,
+    attributes,
+    event: 'input',
+    eventCallback,
+  }).getElement() as HTMLInputElement;
+};
+
+const fieldset = (classNames: string[]): BaseComponentGenerator => {
+  return new BaseComponentGenerator({
+    tag: 'fieldset',
+    classNames,
+  });
+};
+
+const h2 = (
+  classNames: string[],
+  content?: string,
+  attributes: { [key: string]: string } = {},
+): HTMLElement => {
+  return new BaseComponentGenerator({
+    tag: 'h2',
+    classNames,
+    content,
+    attributes,
+  }).getElement();
+};
+
+const img = (
+  classNames: string[],
+  attributes: { [key: string]: string },
+): HTMLElement => {
+  return new BaseComponentGenerator({
+    tag: 'img',
+    classNames,
+    attributes,
+  }).getElement();
+};
+
+const a = (
+  classNames: string[],
+  href: string,
+  content?: string,
+  attributes: { [key: string]: string } = {},
+): BaseComponentGenerator => {
+  return new BaseComponentGenerator({
+    tag: 'a',
+    classNames,
+    content,
+    attributes: { ...attributes, href },
+  });
+};
+
+const p = (
+  classNames: string[],
+  content?: string,
+  attributes: { [key: string]: string } = {},
+): BaseComponentGenerator => {
+  return new BaseComponentGenerator({
+    tag: 'p',
+    classNames,
+    content,
+    attributes,
+  });
+};
+
+const ul = (
+  classNames: string[],
+  attributes: { [key: string]: string } = {},
+): BaseComponentGenerator => {
+  return new BaseComponentGenerator({
+    tag: 'ul',
+    classNames,
+    attributes,
+  });
+};
+
+const li = (
+  classNames: string[],
+  content?: string,
+  attributes: { [key: string]: string } = {},
+): BaseComponentGenerator => {
+  return new BaseComponentGenerator({
+    tag: 'li',
+    classNames,
+    content,
+    attributes,
+  });
+};
+
+const button = (
+  classNames: string[],
+  content: string,
+  attributes: { [key: string]: string } = {},
+  eventCallback: (event?: Event) => void = () => {},
+): HTMLButtonElement => {
+  return new BaseComponentGenerator({
+    tag: 'button',
+    classNames,
+    content,
+    attributes,
+    event: 'click',
+    eventCallback,
+  }).getElement() as HTMLButtonElement;
+};
+
+const legend = (
+  classNames: string[],
+  content: string,
+  attributes: { [key: string]: string } = {},
+): HTMLElement => {
+  return new BaseComponentGenerator({
+    tag: 'legend',
+    classNames,
+    content,
+    attributes,
+  }).getElement();
+};
+const tags = {
+  div,
+  input,
+  fieldset,
+  h2,
+  img,
+  a,
+  p,
+  ul,
+  li,
+  button,
+  legend,
+};
+export default tags;
