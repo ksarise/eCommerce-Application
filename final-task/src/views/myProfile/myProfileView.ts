e86021695e4fb8ca2d17e84235ffe6a931440cc8
Viktoriya Nykytenko <kitakiv@gmail.com>
2024-06-01 23:09:56 +0300

    feat: create block addresses shippind and billing blocks

--- final-task/src/views/myProfile/myProfileView.ts
+++ final-task/src/views/myProfile/myProfileView.ts
@@ -1,13 +1,17 @@
 import Personal from './components/personal';
 import tags from '../../tags/tags';
+import Addresses from './components/addresses';
 
 export default class ProfileView {
   public personalBlock: Personal;
 
   public blockProfile: HTMLElement;
 
+  public addressesBlock: Addresses;
+
   constructor() {
     this.personalBlock = new Personal();
+    this.addressesBlock = new Addresses();
     this.blockProfile = tags.div(['profile']).getElement();
     this.createProfileBlock();
   }
@@ -18,5 +22,6 @@ export default class ProfileView {
 
   createProfileBlock() {
     this.blockProfile.append(this.personalBlock.initPersonal());
+    this.blockProfile.append(...this.addressesBlock.createAddressesBlocks());
   }
 }
